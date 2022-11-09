a1ff9a0155b4986c812ed7e0728e8cebca07f9d9
Olawale Aro <aro.olawale1@gmail.com>
2022-11-09 20:39:06 +0100

    revome controllers file

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+const bookRoute = require('./routes/book.route')
+
+app.use(bodyParser.json())
+app.use(bookRoute)
+
 app.listen(port, ()=>{'server is running'})
